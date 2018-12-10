d48290a4a1a24b7827719d7dc1997a17f24a9061
Gordon Tyler <gordon@doxxx.net>
2018-12-10 11:53:42 -0500

    Day 8, part 1.

---
+++ day8/src/main.rs
@@ -0,0 +1,73 @@
+use std::io::prelude::*;
+use std::iter::FromIterator;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn main() -> Result<()> {
+    let data = read_input()?;
+
+    part1(&data);
+
+    Ok(())
+}
+
+fn read_input() -> Result<Vec<usize>> {
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input)?;
+    Ok(input
+        .trim_end()
+        .split_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect())
+}
+
+fn part1(data: &[usize]) {
+    let tree = build_tree(&mut data.iter().map(|d| *d));
+
+    let result = tree.sum_metadata();
+
+    println!("part1: {}", result);
+}
+
+fn build_tree(data: &mut Iterator<Item = usize>) -> Node {
+    let num_children = data.next().unwrap();
+    let num_metadata = data.next().unwrap();
+    let mut children = Vec::new();
+    for _ in 0..num_children {
+        let child = build_tree(data);
+        children.push(child);
+    }
+    let mut metadata = Vec::new();
+    for _ in 0..num_metadata {
+        metadata.push(data.next().unwrap());
+    }
+    Node { children, metadata }
+}
+
+#[derive(Debug)]
+struct Node {
+    children: Vec<Node>,
+    metadata: Vec<usize>,
+}
+
+impl Node {
+    fn print(&self, indent: usize) {
+        println!(
+            "{}Node: {:?}",
+            String::from_iter(vec![' '; indent].into_iter()),
+            self.metadata
+        );
+        for child in &self.children {
+            child.print(indent + 2);
+        }
+    }
+
+    fn sum_metadata(&self) -> usize {
+        self.metadata.iter().map(|m| *m).sum::<usize>()
+            + self
+                .children
+                .iter()
+                .map(|n| n.sum_metadata())
+                .sum::<usize>()
+    }
+}
