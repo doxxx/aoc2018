4819238d503098bfb90ce34786f0cfe6b0100f70
Gordon Tyler <gordon@doxxx.net>
2019-01-04 19:20:54 -0500

    RefCell abomination

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,17 +1,18 @@
 use shared::Grid;
+use std::cell::RefCell;
 use std::cmp::Ordering;
 use std::io;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let (map, units) = read_input()?;
+    let mut system = read_input()?;
 
-    run(&map, units.clone());
+    system.run();
 
     Ok(())
 }
 
-fn read_input() -> std::io::Result<(Map, Vec<Unit>)> {
+fn read_input() -> std::io::Result<System> {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input)?;
 
@@ -48,36 +49,122 @@ fn read_input() -> std::io::Result<(Map, Vec<Unit>)> {
         }
     }
 
-    Ok((map, units))
+    Ok(System::new(map, units))
 }
 
-fn run(map: &Map, mut units: Vec<Unit>) {
-    for i in 0..3 {
-        println!("Step {}:\n{}", i, combined_map(map, &units));
-
-        // let unit_moves: Vec<Option<Pos>> = units
-        //     .iter()
-        //     .map(|u| u.move_to_nearest_target(map, &units))
-        //     .collect();
-
-        // //units.iter().zip(unit_moves.iter()).collect::<Vec<_>>()
-
-        // units.iter_mut().zip(unit_moves).for_each(|(u, m)| {
-        //     if let Some(m) = m {
-        //         u.apply_move(m)
-        //     }
-        // });
-    }
-
-    println!("Final:\n{}", combined_map(map, &units));
-}
-
-fn combined_map(map: &Map, units: &[Unit]) -> Map {
+fn combined_map(map: &Map, units: &[RefCell<Unit>]) -> Map {
     let mut combined_map = map.clone();
     for unit in units.iter() {
+        let unit = unit.borrow();
         combined_map[(unit.x, unit.y)] = unit.kind;
     }
     combined_map
+}
+
+struct System {
+    map: Map,
+    units: Vec<RefCell<Unit>>,
+}
+
+impl System {
+    fn new(map: Map, units: Vec<Unit>) -> System {
+        System {
+            map,
+            units: units.into_iter().map(RefCell::new).collect(),
+        }
+    }
+
+    fn run(&mut self) {
+        for i in 0..3 {
+            println!("Step {}:\n{}", i, combined_map(&self.map, &self.units));
+
+            for unit in self.units.iter() {
+                if self.move_unit(&unit) {
+                    self.attack(&unit);
+                }
+            }
+        }
+
+        println!("Final:\n{}", combined_map(&self.map, &self.units));
+    }
+
+    fn move_unit(&self, unit: &RefCell<Unit>) -> bool {
+        if self
+            .units
+            .iter()
+            .any(|u| unit.borrow().in_range(&u.borrow()))
+        {
+            return false;
+        }
+
+        let unit_positions: Vec<Pos> = self
+            .units
+            .iter()
+            .map(|u| (u.borrow().x, u.borrow().y))
+            .collect();
+        let open_squares: Vec<Pos> = self
+            .units
+            .iter()
+            .filter(|u| u.borrow().kind != unit.borrow().kind)
+            .flat_map(|u| self.map.open_squares_around(u.borrow().x, u.borrow().y))
+            .filter(|p| !unit_positions.contains(p))
+            .collect();
+
+        if open_squares.is_empty() {
+            return false;
+        }
+
+        let mut paths: Vec<Vec<Pos>> = open_squares
+            .into_iter()
+            .flat_map(|pos| {
+                self.map
+                    .shortest_paths(unit.borrow().x, unit.borrow().y, pos.0, pos.1)
+            })
+            .collect();
+
+        if paths.is_empty() {
+            return false;
+        }
+
+        paths.sort_by_key(|p| p.len());
+        let fewest_steps = paths.first().unwrap().len();
+
+        let mut shortest_paths: Vec<Vec<Pos>> = paths
+            .into_iter()
+            .filter(|p| p.len() == fewest_steps)
+            .collect();
+        shortest_paths.sort_by(|a, b| cmp_pos(a[0], b[0]));
+        let shortest_path = shortest_paths.first().unwrap();
+        let (nx, ny) = shortest_path.first().cloned().unwrap();
+
+        unit.borrow_mut().x = nx;
+        unit.borrow_mut().y = ny;
+
+        true
+    }
+
+    fn attack(&self, unit: &RefCell<Unit>) -> bool {
+        let mut in_range: Vec<&RefCell<Unit>> = self
+            .units
+            .iter()
+            .filter(|u| unit.borrow().in_range(&u.borrow()))
+            .collect();
+        if in_range.is_empty() {
+            return false;
+        }
+
+        in_range.sort_by_key(|u| u.borrow().hp);
+        let fewest_hp = in_range.first().unwrap().borrow().hp;
+
+        let mut targets: Vec<&RefCell<Unit>> = in_range
+            .into_iter()
+            .filter(|u| u.borrow().hp == fewest_hp)
+            .collect();
+        targets.sort_by(|a, b| cmp_pos((a.borrow().x, a.borrow().y), (b.borrow().x, b.borrow().y)));
+        targets[0].borrow_mut().hp -= unit.borrow().ap;
+
+        true
+    }
 }
 
 type Map = Grid<char>;
@@ -213,65 +300,5 @@ impl Unit {
     fn in_range(&self, other: &Unit) -> bool {
         (self.x == other.x && (self.y - 1 == other.y || self.y + 1 == other.y))
             || (self.y == other.y && (self.x - 1 == other.x || self.x + 1 == other.x))
-    }
-
-    fn move_to_nearest_target(&mut self, map: &Map, units: &[Unit]) -> bool {
-        if units.iter().any(|u| self.in_range(u)) {
-            return false;
-        }
-
-        let unit_positions: Vec<Pos> = units.iter().map(|u| (u.x, u.y)).collect();
-        let targets: Vec<&Unit> = units.iter().filter(|u| u.kind != self.kind).collect();
-        let open_squares: Vec<Pos> = targets
-            .iter()
-            .flat_map(|u| map.open_squares_around(u.x, u.y))
-            .filter(|p| !unit_positions.contains(p))
-            .collect();
-
-        if open_squares.is_empty() {
-            return false;
-        }
-
-        let mut paths: Vec<Vec<Pos>> = open_squares
-            .into_iter()
-            .flat_map(|pos| map.shortest_paths(self.x, self.y, pos.0, pos.1))
-            .collect();
-
-        if paths.is_empty() {
-            return false;
-        }
-
-        paths.sort_by_key(|p| p.len());
-        let fewest_steps = paths.first().unwrap().len();
-
-        let mut shortest_paths: Vec<Vec<Pos>> = paths
-            .into_iter()
-            .filter(|p| p.len() == fewest_steps)
-            .collect();
-        shortest_paths.sort_by(|a, b| cmp_pos(a[0], b[0]));
-        let shortest_path = shortest_paths.first().unwrap();
-        let (nx, ny) = shortest_path.first().cloned().unwrap();
-
-        self.x = nx;
-        self.y = ny;
-
-        true
-    }
-
-    fn attack(&self, units: &mut [Unit]) -> Option<(Pos, usize)> {
-        let mut in_range: Vec<&mut Unit> = units.iter_mut().filter(|u| self.in_range(u)).collect();
-        if in_range.is_empty() {
-            return None;
-        }
-
-        in_range.sort_by_key(|u| u.hp);
-        let fewest_hp = in_range.first().unwrap().hp;
-
-        let mut targets: Vec<&mut Unit> =
-            in_range.into_iter().filter(|u| u.hp == fewest_hp).collect();
-        targets.sort_by(|a, b| cmp_pos((a.x, a.y), (b.x, b.y)));
-        targets[0].hp -= self.ap;
-
-        Some(((target.x, target.y), self.ap))
     }
 }
